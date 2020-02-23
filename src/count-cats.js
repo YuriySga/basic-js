b32dd61d4533916b36b042da5087a3c0a588290f
YuriySga <ess1928@yandex.ru>
2020-02-23 20:23:54 +0400

    feat: update count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(arr) {  
+  let cat = 0;
+  arr.forEach(function(item) {        
+    if (Array.isArray(item)) {                 
+      item.forEach( function (item1) {                
+        if (item1==='^^') {cat += 1;}                
+      });  
+    } else if (item==='^^') {cat += 1;}        
+  }); 
+  return(cat);
+} 
