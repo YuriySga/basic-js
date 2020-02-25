79dbe621cd324bf64a3b7fc71b610e69bd5a1f59
YuriySga <ess1928@yandex.ru>
2020-02-25 23:10:08 +0400

    feat: update transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,37 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+   if (Array.isArray(arr) !== true) throw new Error();
+   let newArr = [] ;
+   let command = "";    // next command
+   arr.forEach((element, index) => {
+    if (index == 0) {   // if first element of array
+        if (element !== '--discard-prev' && element !== '--double-prev') {
+            if (element === '--double-next' || element === '--discard-next') {
+                command = element;
+            } else {newArr.push(element);}           
+        }
+    } else {          // if NOT first element of array
+        if (element !== '--discard-prev' && element !== '--double-prev' && element !== '--double-next' && element !== '--discard-next') {
+            if (command == "") {
+                newArr.push(element);
+            } else if (command == "--double-next") {
+                newArr.push(element);
+                newArr.push(element);
+                command = "";
+            }  else if (command == "--discard-next") {
+                command = "";
+            }
+        } else {
+            if (element === '--discard-prev') {
+                newArr.pop();
+                command = "";
+            } else if (element === '--double-prev') {
+                newArr.push(arr[index-1]);
+                command = "";
+            } else if (element === '--double-next' || element === '--discard-next') {
+                command = element;                
+            }
+        }
+    }  
+   });
+return newArr;
 };
