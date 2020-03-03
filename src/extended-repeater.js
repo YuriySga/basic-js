88d993035a3f082bf22f8fddf63eb8dbcfb3820f
YuriySga <ess1928@yandex.ru>
2020-03-03 21:58:47 +0400

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,31 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, ob) {    
+        let strRep = "";
+        let myAddit = "";
+        ob.repeatTimes = (ob.repeatTimes === undefined) ? 1 : ob.repeatTimes; 
+        ob.separator = (ob.separator === undefined) ? '+' : ob.separator; 
+        ob.addition = (ob.addition === undefined) ? '' : ob.addition; 
+        ob.additionRepeatTimes = (ob.additionRepeatTimes === undefined) ? 1 : ob.additionRepeatTimes; 
+        ob.additionSeparator = (ob.additionSeparator === undefined) ? '' : ob.additionSeparator;
+      
+        if (ob.additionRepeatTimes !== 0) {
+            while (ob.additionRepeatTimes > 0) {
+                ob.additionRepeatTimes -= 1;
+                myAddit = myAddit + ob.addition;
+                if (ob.additionRepeatTimes > 0) {
+                    myAddit = myAddit + ob.additionSeparator;
+                }
+            }
+        }     
+     
+        while (ob.repeatTimes > 0) {
+            ob.repeatTimes -= 1;
+            strRep = strRep + str + myAddit;
+            if (ob.repeatTimes > 0) {
+                strRep = strRep + ob.separator;
+            }
+        }   
+         
+    return strRep;
+     
 };
   
